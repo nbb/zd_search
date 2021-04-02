a59048bd8769348b9efb4a1d026cff713e588da2
Nick Binnington <nick.binnington@gmail.com>
2021-04-02 21:25:15 +1100

    Seperate code into different files

---
+++ lib/interface.rb
@@ -0,0 +1,84 @@
+class Interface
+  def initialize(data)
+    @data = data
+    @entity, @field, @search_term = nil
+  end
+
+  def call
+    display_introduction
+    display_help
+    input_loop
+  end
+
+  private
+
+  def input_loop
+    if !@entity
+      puts "\nReady to search? y/n\n"
+      exit if gets.chomp.downcase != "y"
+
+      entity_options = ["users", "tickets", "organizations"]
+      puts "\nWhich entity would you like to search?"
+      puts "\nSelect a number:\n"
+      entity_options.each_with_index { |option, i| puts "#{i+1}) #{option.capitalize}" }
+
+      @entity = get_input(blank_allowed: false)
+      puts "\nSearching #{entity_options[@entity.to_i - 1]}..." if @entity
+      input_loop
+    end
+
+    if !@field
+      puts "\nEnter the field you'd like to search:\n"
+      @field = get_input(blank_allowed: false)
+      input_loop
+    end
+
+    if !@search_term
+      puts "\nEnter your search term:\n"
+      @search_term = get_input(blank_allowed: true)
+      input_loop
+    end
+  end
+
+  def display_introduction
+    puts "\nWelcome to Zendesk Search!\n"
+  end
+
+  def display_help
+    puts "\nAvailable commands:\n\n"
+    puts "      \\exit — exits"
+    puts "      \\restart — starts again"
+    puts "      \\fields — views a list searchable fields"
+    puts "      \\help — see these commands again\n"
+  end
+
+  def display_fields
+    @data.keys.each do |entity_name|
+      puts "\n#{entity_name.capitalize}:"
+      puts "\n  #{@data[entity_name].first.keys.join(", ")}"
+    end
+  end
+
+  def get_input(blank_allowed: false)
+    input = gets.chomp
+    if input == "" && !blank_allowed
+      puts "! You must enter a value here !"
+      return nil
+    end
+
+    case input
+    when "\\exit"
+      exit
+    when "\\help"
+      display_help
+      nil
+    when "\\fields"
+      display_fields
+      nil
+    when "\\restart"
+      @entity, @field, @search_term = nil
+    else
+      input
+    end
+  end
+end
