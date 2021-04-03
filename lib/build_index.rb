4021a6ed309d6f44c7785fc83177493bfaf54a09
Nick Binnington <nick.binnington@gmail.com>
2021-04-03 14:25:26 +1100

    Move json loading out of the BuildIndex class

--- lib/build_index.rb
+++ lib/build_index.rb
@@ -13,19 +13,16 @@
 #   }
 # }
 class BuildIndex
-  def call
-    data = {}
-    file_names = ["users.json", "tickets.json", "organizations.json"]
-    file_names.each do |file_name|
-      file = File.read("data/" + file_name)
-      data[file_name.gsub(".json", "").to_sym] = JSON.parse(file)
-    end
+  def initialize(data)
+    @data = data
+  end
 
+  def call
     # Can we initialise the inverted index using the data hash above instead?
     inverted_index = {
       users: {
         _id: {}, url: {}, external_id: {}, name: {}, alias: {}, created_at: {}, active: {}, verified: {}, shared: {}, locale: {}, timezone: {}, last_login_at: {}, email: {}, phone: {}, signature: {}, organization_id: {}, tags: {}, suspended: {}, role: {}
-      }, 
+      },
       tickets: {
         _id: {}, url: {}, external_id: {}, created_at: {}, type: {}, subject: {}, description: {}, priority: {}, status: {}, submitter_id: {}, assignee_id: {}, organization_id: {}, tags: {}, has_incidents: {}, due_at: {}, via: {}
       },
@@ -36,14 +33,15 @@ class BuildIndex
 
     # Build out our inverted index here
     # I think this needs to be recursive and follow the structure of the data input
-    data.keys.each do |key|
-      data[key].each_with_index do |item, i|
-        inverted_index[key].keys.each do |index_key|
-          inverted_index[key][index_key][item[index_key.to_s]] = i
+    @data.keys.each do |key|
+      @data[key].each_with_index do |item, i|
+        # byebug
+        inverted_index[key.to_sym].keys.each do |index_key|
+          inverted_index[key.to_sym][index_key][item[index_key.to_s]] = i
         end
       end
     end
 
-    return data
+    return @data
   end
 end
--- zendesk_search.rb
+++ zendesk_search.rb
@@ -3,7 +3,28 @@ require "json"
 require_relative "lib/build_index"
 require_relative "lib/interface"
 
-build_index = BuildIndex.new
+json_files = [
+  {
+    entity_name: "users",
+    relative_path: "data/users.json"
+  },
+  {
+    entity_name: "tickets",
+    relative_path: "data/tickets.json"
+  },
+  {
+    entity_name: "organizations",
+    relative_path: "data/organizations.json"
+  },
+]
+
+data = {}
+json_files.each do |json_file|
+  file = File.read(json_file[:relative_path])
+  data[json_file[:entity_name]] = JSON.parse(file)
+end
+
+build_index = BuildIndex.new(data)
 indexed_data = build_index.call
 
 interface = Interface.new(indexed_data)
